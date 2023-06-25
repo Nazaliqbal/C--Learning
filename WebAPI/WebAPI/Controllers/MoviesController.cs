8a333c09bce202b2d7ee0fa1a54d40c9652377c1
Nazaliqbal <nazal.iqbal@codecraft.co.in>
2023-06-25 14:19:22 +0530

    Creared ,net core 6.0 web api project

---
+++ WebAPI/WebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _dbContext;
+
+        public MoviesController(MovieContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        //get all movies method
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetAllMovies()
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _dbContext.Movies.ToListAsync();
+            var formattedMovies = movies.Select(movie =>
+            {
+                movie.ReleaseDate = movie.ReleaseDate.Date; // Optionally remove the time component
+                return movie;
+            });
+
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters = new List<JsonConverter> { new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" } }
+            };
+
+            var json = JsonConvert.SerializeObject(formattedMovies, settings);
+            var result = new ContentResult
+            {
+                Content = json,
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+
+            return result;
+        }
+        //get movie by id method
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            movie.ReleaseDate = movie.ReleaseDate.Date; // Optionally remove the time component
+
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters = new List<JsonConverter> { new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" } }
+            };
+
+            var json = JsonConvert.SerializeObject(movie, settings);
+            var result = new ContentResult
+            {
+                Content = json,
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+
+            return result;
+        }
+        //post method
+        [HttpPost]
+        public async Task<ActionResult<Movie>> CreateMovie([FromBody] Movie movie)
+        {
+            if (_dbContext.Movies.Any(m => m.Title == movie.Title))
+            {
+                return Conflict("A movie with the same title already exists.");
+            }
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+        }
+        //update method
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, [FromBody] Movie updatedMovie)
+        {
+            var existingMovie = await _dbContext.Movies.FindAsync(id);
+            if (existingMovie == null)
+            {
+                return NotFound();
+            }
+
+            // Update the properties of the existing movie
+            existingMovie.Title = updatedMovie.Title;
+            existingMovie.Genre = updatedMovie.Genre;
+            existingMovie.ReleaseDate = updatedMovie.ReleaseDate;
+
+            _dbContext.Entry(existingMovie).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+        //delete method
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _dbContext.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Movies.Remove(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+    }
+}
